7dd79fc581a5fe54f0fb78f9e3d592178775ebdb
Junlang-7 <1448760937@qq.com>
2026-03-27 17:43:09 +0800

    lab4 Part A: replicated state machine (RSM)

--- src/kvraft1/rsm/rsm.go
+++ src/kvraft1/rsm/rsm.go
@@ -1,22 +1,22 @@
 package rsm
 
 import (
+	"math/rand"
 	"sync"
+	"time"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/raft1"
+	raft "6.5840/raft1"
 	"6.5840/raftapi"
-	"6.5840/tester1"
-
+	tester "6.5840/tester1"
 )
 
 type Op struct {
-	// Your definitions here.
-	// Field names must start with capital letters,
-	// otherwise RPC will break.
+	Me  int
+	Req any
+	ID  uint64
 }
-
 
 // A server (i.e., ../server.go) that wants to replicate itself calls
 // MakeRSM and must implement the StateMachine interface.  This
@@ -31,13 +31,23 @@ type StateMachine interface {
 }
 
 type RSM struct {
-	mu           sync.Mutex
-	me           int
-	rf           raftapi.Raft
-	applyCh      chan raftapi.ApplyMsg
-	maxraftstate int // snapshot if log grows this big
-	sm           StateMachine
-	// Your definitions here.
+	mu               sync.Mutex
+	me               int
+	rf               raftapi.Raft
+	applyCh          chan raftapi.ApplyMsg
+	maxraftstate     int // snapshot if log grows this big
+	sm               StateMachine
+	epoch            uint64
+	nexSeq           uint64
+	dead             bool                         // applyCh is closed when this is true
+	waitChByOpID     map[uint64]chan submitResult // map from op ID to channel for delivering result
+	pendingLogByOpID map[int]uint64               // map from log index to op ID
+}
+
+// submitResult represents the result of a submitted operation
+type submitResult struct {
+	err rpc.Err
+	rep any
 }
 
 // servers[] contains the ports of the set of
@@ -56,22 +66,67 @@ type RSM struct {
 // MakeRSM() must return quickly, so it should start goroutines for
 // any long-running work.
 func MakeRSM(servers []*labrpc.ClientEnd, me int, persister *tester.Persister, maxraftstate int, sm StateMachine) *RSM {
+	rng := rand.New(rand.NewSource(time.Now().UnixNano() + int64(me)*1_000_000))
 	rsm := &RSM{
-		me:           me,
-		maxraftstate: maxraftstate,
-		applyCh:      make(chan raftapi.ApplyMsg),
-		sm:           sm,
+		me:               me,
+		maxraftstate:     maxraftstate,
+		applyCh:          make(chan raftapi.ApplyMsg),
+		sm:               sm,
+		waitChByOpID:     make(map[uint64]chan submitResult),
+		pendingLogByOpID: make(map[int]uint64),
+		epoch:            rng.Uint64(),
 	}
 	if !tester.UseRaftStateMachine {
 		rsm.rf = raft.Make(servers, me, persister, rsm.applyCh)
 	}
+	go rsm.reader()
 	return rsm
+}
+
+func (rsm *RSM) reader() {
+	for msg := range rsm.applyCh {
+		if msg.CommandValid {
+			op := msg.Command.(Op)
+			rep := rsm.sm.DoOp(op.Req)
+
+			rsm.mu.Lock()
+			// Check if this index has a pending request
+			if expectID, ok := rsm.pendingLogByOpID[msg.CommandIndex]; ok {
+				if expectID == op.ID {
+					// This is the expected request, deliver the result
+					if ch, exists := rsm.waitChByOpID[op.ID]; exists {
+						ch <- submitResult{rpc.OK, rep}
+						// move out from pending list
+						delete(rsm.waitChByOpID, op.ID)
+					}
+				} else {
+					// Different request got committed at this index
+					// The original request must have lost leadership
+					if ch, exists := rsm.waitChByOpID[op.ID]; exists {
+						ch <- submitResult{rpc.ErrWrongLeader, rep}
+						delete(rsm.waitChByOpID, op.ID)
+					}
+				}
+				delete(rsm.pendingLogByOpID, msg.CommandIndex)
+			}
+			rsm.mu.Unlock()
+		}
+	}
+
+	// applyCh is closed, wake up all pending requests
+	rsm.mu.Lock()
+	defer rsm.mu.Unlock()
+	rsm.dead = true
+	for _, ch := range rsm.waitChByOpID {
+		ch <- submitResult{rpc.ErrWrongLeader, nil}
+	}
+	rsm.waitChByOpID = make(map[uint64]chan submitResult)
+	rsm.pendingLogByOpID = make(map[int]uint64)
 }
 
 func (rsm *RSM) Raft() raftapi.Raft {
 	return rsm.rf
 }
-
 
 // Submit a command to Raft, and wait for it to be committed.  It
 // should return ErrWrongLeader if client should find new leader and
@@ -82,6 +137,51 @@ func (rsm *RSM) Submit(req any) (rpc.Err, any) {
 	// for example: op := Op{Me: rsm.me, Id: id, Req: req}, where req
 	// is the argument to Submit and id is a unique id for the op.
 
-	// your code here
-	return rpc.ErrWrongLeader, nil // i'm dead, try another server.
+	rsm.mu.Lock()
+	op := Op{Me: rsm.me, Req: req, ID: rsm.epoch<<32 | rsm.nexSeq}
+	rsm.nexSeq++
+	rsm.mu.Unlock()
+
+	index, startTerm, isLeader := rsm.rf.Start(op)
+	// if not the leader, return ErrWrongLeader immediately
+	if !isLeader {
+		return rpc.ErrWrongLeader, nil
+	}
+
+	ch := make(chan submitResult, 1)
+	rsm.mu.Lock()
+	// If applyCh closed, exit out of all loops.
+	if rsm.dead {
+		rsm.mu.Unlock()
+		return rpc.ErrWrongLeader, nil
+	}
+	// Record the pending request with its log index and op ID
+	rsm.pendingLogByOpID[index] = op.ID
+	rsm.waitChByOpID[op.ID] = ch
+	rsm.mu.Unlock()
+
+	// Periodic ticker to check if we've lost leadership
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case res := <-ch:
+			// Successfully received result from reader goroutine
+			return res.err, res.rep
+		case <-ticker.C:
+			// Periodically check if we've lost leadership
+			rsm.mu.Lock()
+			currentTerm, isLeader := rsm.rf.GetState()
+			// detect that it has lost leadership by noticing that Raft's term has changed
+			if currentTerm != startTerm || !isLeader {
+				// Leadership has changed, clean up and return error
+				delete(rsm.pendingLogByOpID, index)
+				delete(rsm.waitChByOpID, op.ID)
+				rsm.mu.Unlock()
+				return rpc.ErrWrongLeader, nil
+			}
+			rsm.mu.Unlock()
+		}
+	}
 }
